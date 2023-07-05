03fb5393ae135f8f7c78d4e72313caa5e883cdd0
Derek <48260574+DerekHLeckner@users.noreply.github.com>
2023-07-04 22:22:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from transformers import pipeline
 from textblob import TextBlob
 
 # Set the page title
@@ -7,20 +8,15 @@ st.title("Sentiment Analysis App")
 # Create a textarea for user input
 text = st.text_area("Enter text")
 
+classifier = pipeline('sentiment-analysis')
 # Create a button to perform sentiment analysis
 if st.button("Analyze"):
     # Perform sentiment analysis
-    blob = TextBlob(text)
+    blob = classifier(text)
     sentiment = blob.sentiment.polarity
 
     # Determine sentiment label
-    if sentiment > 0:
-        sentiment_label = "Positive"
-    elif sentiment < 0:
-        sentiment_label = "Negative"
-    else:
-        sentiment_label = "Neutral"
-
+    sentiment_label = sentiment[0].label
     # Display the sentiment analysis results
     st.write("Sentiment:", sentiment)
     st.write("Sentiment Label:", sentiment_label)
